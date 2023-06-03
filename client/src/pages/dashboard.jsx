1557a15425dd953a357e05e6870c1d51534df754
AsumVictor <iamasum369@gmail.com>
2023-06-03 15:50:56 +0000

    Add card to show user progress

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HiChevronRight } from "react-icons/hi2";
-
+import { HiViewGrid , HiOutlineDotsVertical} from "react-icons/hi"
 function Dashboard() {
   return (
     <div className="w-full py-2 mt-10 px-3 md:px-10 pb-20">
@@ -16,9 +16,16 @@ function Dashboard() {
 <div className="w-full  py-2 mt-10 flex justify-center gap-5 flex-wrap">
    
 <div className="w-[20rem] h-[15rem] md:w-[25rem] rounded-2xl bg-darkSecondary">
-  <div className="w-full py-2 grid grid-cols-12 px-2">
-     <div className="h-[3.6rem] w-[3.6rem] bg-darkTertiary rounded-2xl flex justify-center items-center">
-     <HiChevronRight />
+  <div className="w-full py-2 grid grid-cols-12 px-2 gap-3">
+     <div className="h-[3rem] w-[3rem] bg-darkTertiary rounded-2xl flex justify-center items-center text-3xl text-pinkPrimary col-span-2">
+     <HiViewGrid />
+     </div>
+     <div className=" rounded-2xl flex justify-start items-center text-[20px] text-darkTextPrimary col-span-9 font-semibold">
+     My task progress
+     </div>
+
+     <div className="rounded-2xl flex justify-center items-center text-[20px] text-darkTextPrimary col-span-1">
+     <HiOutlineDotsVertical />
      </div>
   </div>
 </div>
