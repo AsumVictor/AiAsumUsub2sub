f543056840267364bfb62988aa51109c4ff96cfd
AsumVictor <iamasum369@gmail.com>
2023-06-05 11:30:07 +0000

    Add modal box component, link moidal box to task page

--- client/src/component/modalBox.jsx
+++ client/src/component/modalBox.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function ModalBox() {
+function ModalBox({ title, children, footer }) {
+
   return (
-    <div>ModalBox</div>
-  )
+    <>
+   <div className="fixed top-0 left-0 w-full h-full overflow-x-hidden overflow-y-auto flex justify-center items-center z-[99] backdrop-blur-md">
+      <div className="flex bg-darkSecondary flex-col shadow-2xl min-w-[20rem] max-w-[35rem] md:w-[35rem] py-2 shadow-white px-1 md:px-5">
+        <h2 className="text-2xl text-darkTextPrimary text-center font-semibold">
+          {title}
+        </h2>
+        <div className=" bg-darkTertiary w-full h-[3px] mt-2"></div>
+        <div className="">{children}</div>
+        <div className=" bg-darkTertiary w-full h-[3px] mt-2"></div>
+       {footer}
+      </div>
+    </div>
+    
+    </>
+  );
 }
 
-export default ModalBox
+export default ModalBox;
+
+export function ModalFooter({children}){
+  return (
+    <div className="flex w-full flex-row gap-x-2 justify-end mt-2">
+    {children}
+  </div>
+  )
+}
--- client/src/pages/portal.jsx
+++ client/src/pages/portal.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiChevronRight, HiSquare3Stack3D } from "react-icons/hi2";
 import {
   HiViewGrid,
   HiOutlineDotsVertical,
   HiTrendingUp,
   HiChartBar,
-  HiPlusSm
+  HiPlusSm,
 } from "react-icons/hi";
+import ModalBox, { ModalFooter } from "../component/modalBox";
 
 function Portal() {
+  const [showModal, setShowModal] = useState(false);
+  const [url, setURL] = useState('');
+  const [isValid, setValid] = useState(false);
+  const [link, setLink] =useState('')
+  const [youtubeName, setYoutubeName] =useState('')
+
+
+  const handleLinkChange = (event) => {
+    const enteredURL = event.target.value;
+    setLink(enteredURL)
+    const pattern = /https:\/\/www\.youtube\.com\/channel\/[A-Za-z0-9_-]+/;
+    const isValidURL = pattern.test(enteredURL);
+    setURL(enteredURL);
+    setValid(isValidURL);
+  };
+
   return (
     <div className="w-full py-2 mt-10 px-3 md:px-10 pb-20">
       <h1 className="text-[18px] font-semibold text-darkTextPrimary">
@@ -21,7 +38,7 @@ function Portal() {
           <span>Link activities</span> <HiChevronRight />
         </h2>
         <div className="w-1/2 flex justify-end">
-          <button className="py-2 px-4 bg-pinkPrimary rounded-md font-bold text-white flex flex-row gap-1 items-center">
+          <button className="py-2 px-4 bg-pinkPrimary rounded-md font-bold text-white flex flex-row gap-1 items-center"  onClick={() => setShowModal(true)}>
             <span className="text-3xl">
               <HiPlusSm />
             </span>
@@ -30,7 +47,6 @@ function Portal() {
         </div>
       </div>
 
-      
       <div className="w-full py-2 mt-10 flex justify-start gap-5 flex-wrap">
         <div className="w-[20rem] h-[15rem] md:w-[25rem] rounded-2xl bg-darkSecondary px-3">
           <div className="w-full py-2 grid grid-cols-12 gap-3">
@@ -68,17 +84,65 @@ function Portal() {
       </div>
 
       <h2 className="mt-[55px] flex gap-3 px-3 items-center text-darkTextPrimary font-semibold text-[20px]">
-          <span>All my youtube links</span> <HiChevronRight />
-        </h2>
+        <span>All my youtube links</span> <HiChevronRight />
+      </h2>
 
-<div className="w-full flex flex-col gap-5 py-3 mt-10">
-  <div className="grid grid-cols-10 bg-darkSecondary rounded-xl w-full md:w-[40rem] items-center gap-2 px-2 py-2">
-     <span className="col-span-1 w-[2rem] h-[2rem] rounded-full bg-pinkPrimary text-white flex justify-center items-center font-bold">1</span>
-     <h4 className=" col-span-7 text-white font-semibold">The magic chrome book of the death</h4>
-     <h4 className="col-span-2 text-[12px] font-bold text-darkTextPrimary">2 days ago</h4>
-  </div>
-</div>
+      <div className="w-full flex flex-col gap-5 py-3 mt-10">
+        <div className="grid grid-cols-10 bg-darkSecondary rounded-xl w-full md:w-[40rem] items-center gap-2 px-2 py-2">
+          <span className="col-span-1 w-[2rem] h-[2rem] rounded-full bg-pinkPrimary text-white flex justify-center items-center font-bold">
+            1
+          </span>
+          <h4 className=" col-span-7 text-white font-semibold">
+            The magic chrome book of the death
+          </h4>
+          <h4 className="col-span-2 text-[12px] font-bold text-darkTextPrimary">
+            2 days ago
+          </h4>
+        </div>
+      </div>
 
+      {showModal && (
+        <ModalBox
+          title={"Add your youtube link"}
+          footer={
+            <ModalFooter>
+              <button
+                className="py-1 text-pinkPrimary font-bold px-2"
+                onClick={() => setShowModal(false)}
+              >
+                CANCEL
+              </button>
+              <button className="py-1 bg-emerald-600 font-bold px-3 rounded-md text-white">
+                Add link
+              </button>
+            </ModalFooter>
+          }
+        >
+          <div className="w-full mt-1 ">
+            <p className="font-semibold text-darkTextPrimary">
+              * Youtube Channel name
+            </p>
+            <input
+              type="text"
+              name="youtubeName"
+              placeholder="AiAsum Sub2Sub"
+                            onChange={(e)=>setYoutubeName(e.target.value)}
+              className="w-full mt-1 h-[2rem] rounded-md outline-0 border-2 bg-darkSecondary border-darkTextPrimary text-darkTextPrimary px-2 font-semibold placeholder-darkTertiary"
+            />
+             <p className="font-semibold text-darkTextPrimary mt-4">
+              * Link to youtube channel
+            </p>
+            <input
+              type="text"
+              name="youtubeLink"
+              onChange={(event)=>handleLinkChange(event)}
+              placeholder="https://www.youtube.com/channel/UCiUNFyQ6F-6XfleCjnXJanA"
+              className="w-full mt-1 h-[2rem] rounded-md outline-0 border-2 bg-darkSecondary border-darkTextPrimary text-darkTextPrimary px-2 font-semibold placeholder-darkTertiary placeholder:text-[14px]"
+            />
+             {(!isValid && link ) ? <p className="text-red-600 font-semibold">* URL is invalid.</p> : ''}
+          </div>
+        </ModalBox>
+      )}
     </div>
   );
 }
